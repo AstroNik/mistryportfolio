67a87de9983be42fd3218e0d944b217c540dfc44
Nikhil <nikhil.kapadia.97@gmail.com>
2021-03-08 18:34:28 -0500

    small change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -115,6 +115,7 @@ const About = () => {
                         <ImageGallery
                             items={images}
                             showPlayButton={false}
+                            showFullscreenButton={false}
                         />
                     </div>
                 </div>
