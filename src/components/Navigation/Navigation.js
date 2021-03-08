82061a50d3084006141db9dc30234c4a85ca4f64
Nikhil <nikhil.kapadia.97@gmail.com>
2021-03-08 18:56:09 -0500

    Fixed padding issues

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ class Navigation extends Component {
                 <Navbar className="justify-content-center pb-0 pt-0 text-center" sticky="top">
                     <Navbar.Brand id="nav-title" href="/">Mistry Design</Navbar.Brand>
                 </Navbar>
-                <Navbar className="text-center pt-0 pb-1 navigation" sticky="top">
+                <Navbar className="text-center pt-0 pb-2 navigation" sticky="top">
                     <div className="nav-links">
                         <NavTab exact to="/" className="link-click" activeClassName="link-active">Projects</NavTab>
                         <NavTab to="/about" className="link-click" activeClassName="link-active">About</NavTab>
