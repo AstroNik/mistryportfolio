8b251a8e9fa2fb5633539218b4bcb20a496aabf7
Nikhil <nikhil.kapadia.97@gmail.com>
2021-03-08 18:33:06 -0500

    Added underline for active links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,12 +8,14 @@ class Navigation extends Component {
     render() {
         return (
             <header className="dropshadow">
-                <Navbar className="justify-content-center pb-0 text-center" sticky="top">
+                <Navbar className="justify-content-center pb-0 pt-0 text-center" sticky="top">
                     <Navbar.Brand id="nav-title" href="/">Mistry Design</Navbar.Brand>
                 </Navbar>
-                <Navbar className="text-center p-0 navigation" sticky="top">
-                    <NavTab exact to="/" className="w-100 link-click">Projects</NavTab>
-                    <NavTab to="/about" className="w-100 link-click">About</NavTab>
+                <Navbar className="text-center pt-0 pb-1 navigation" sticky="top">
+                    <div className="nav-links">
+                        <NavTab exact to="/" className="link-click" activeClassName="link-active">Projects</NavTab>
+                        <NavTab to="/about" className="link-click" activeClassName="link-active">About</NavTab>
+                    </div>
                 </Navbar>
                 <SmallNav/>
             </header>
